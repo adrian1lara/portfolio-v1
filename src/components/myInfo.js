f916d90c068dafe7322979a20308e366402316b0
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-20 17:42:34 -0500

    update text

--- src/components/myInfo.js
+++ src/components/myInfo.js
@@ -1,8 +1,10 @@
 export default function MyInfo() {
     return (
         <div>
-            <p>I am a FullStack Developer based in Lima, Peru. I really enjoy developing fullstack apps and websites. My currently hobby is surf,
-                catch some waves actually is my pasion too.
+            <p>
+            I'm a passionate Full-Stack Developer from Lima, Peru.  With a knack for problem-solving and 
+            a drive to create impactful solutions, I leverage my skills in 
+            Javascript, Sql and NoSQL  to build user-friendly web applications.
             </p>
         </div>
     )
