0c1a0af2e73fce614a722e487ec120875f0a4d8f
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-20 17:41:27 -0500

    add flex-col

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 max-w-8xl">
-      <div className="flex ">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 px-10 max-w-[800px] border-2">
+      <div className="flex flex-col">
           <LogoName />
           <MyInfo/>
       </div>
