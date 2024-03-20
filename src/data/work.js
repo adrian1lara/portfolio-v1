38fc6664bb06711d67ca1e53d0f1d5dfd146d4e4
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-20 00:25:25 -0500

    add work array

---
+++ src/data/work.js
@@ -0,0 +1,18 @@
+import chatty from "../assets/chatty.png"
+export const work = [
+    {
+        title: 'chatty',
+        img: chatty,
+        description: "Chat Web App in real time"
+    },
+    {
+        title: 'Bloggy',
+        img: chatty,
+        description: "Personal blog",
+    },
+    {
+        title: 'MembersOnly',
+        img: chatty,
+        description: "Web to post content and auth as a member"
+    }
+]
