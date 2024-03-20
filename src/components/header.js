62c151600fc5d2c3f668db6bd021a7c1254d9329
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-20 17:41:54 -0500

    add hover to my links

--- src/components/header.js
+++ src/components/header.js
@@ -2,22 +2,22 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="md:mt-10 w-full fixed max-w-[800px] border-2 ">
+        <header className="flex  w-full fixed max-w-[800px] justify-center p-3 border-2">
             <nav>
                 <ul className="flex space-x-2">
                     <li>
-                        <Link href={"/"}>
+                        <Link href={"/"} className="hover:border-b-2 ease-in duration-200 border-[#183D3D]">
                             Home
                         </Link>
                     </li>
                     <li>
-                        <Link href={"/work"}>
-                            projects
+                        <Link href={"/work"} className="hover:border-b-2 ease-in duration-200 border-[#183D3D]" >
+                            Work
                         </Link>
                         
                     </li>
                     <li>
-                        <Link href={"/posts"}>
+                        <Link href={"/posts"} className="hover:border-b-2 ease-in duration-200 border-[#183D3D]">
                             Posts
                         </Link>
                     </li>
