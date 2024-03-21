6c0bdbf2211b71dcef412043d6d4f541ddf0d047
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-21 10:23:20 -0500

    update text and add styles

--- src/components/myInfo.js
+++ src/components/myInfo.js
@@ -1,10 +1,11 @@
 export default function MyInfo() {
     return (
-        <div>
-            <p>
+        <div className="mt-10">
+            <h3 className="border-b-2 border-green-300 max-w-fit">About me</h3>
+            <p className="mt-4">
             I'm a passionate Full-Stack Developer from Lima, Peru.  With a knack for problem-solving and 
-            a drive to create impactful solutions, I leverage my skills in 
-            Javascript, Sql and NoSQL  to build user-friendly web applications.
+            a drive to create impactful solutions with code, I love to build user-friendly web applications.
+            When I am not coding I usually surf or create content to my YouTube channel "devLara".
             </p>
         </div>
     )
