f0eebbb6c83cd2e106ced7245c0543383949750c
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 00:17:14 -0500

    add Find me and contact me component

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
+import ContactMe from "@/components/contact";
+import FindMe from "@/components/findme";
 import LogoName from "@/components/logoName";
 import MyInfo from "@/components/myInfo";
 import MyStack from "@/components/stack";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-24 px-10 max-w-[800px] border-2">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24 px-10 max-w-[800px] ">
       <div className="flex flex-col">
           <LogoName />
           <MyInfo/>
           <MyStack/>
+          <FindMe />
+          <ContactMe/>
       </div>
     </main>
   );
