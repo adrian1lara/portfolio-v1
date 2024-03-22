b8f81621731089ce5df2316d657dd28a4bca6eb7
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 00:19:19 -0500

    update h3 styles

--- src/components/myInfo.js
+++ src/components/myInfo.js
@@ -1,7 +1,7 @@
 export default function MyInfo() {
     return (
         <div className="mt-10">
-            <h3 className="border-b-2 border-green-300 max-w-fit">About me</h3>
+            <h3 className="border-b-2 border-green-300 border-opacity-50 max-w-fit">About me</h3>
             <p className="mt-4">
             I'm a passionate Full-Stack Developer from Lima, Peru.  With a knack for problem-solving and 
             a drive to create impactful solutions with code, I love to build user-friendly web applications.
