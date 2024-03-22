b99f891db88f05470bbe9bf6265fb51f726530c6
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 00:19:05 -0500

    create logo component

---
+++ src/components/logo.js
@@ -0,0 +1,7 @@
+import { FaCode } from "react-icons/fa";
+
+export default function Logo({font}) {
+    return (
+        <h1 className={`font-bold capitalize flex items-center`}><FaCode className="text-green-400  mr-1"/> adrian lara</h1>
+    )
+}
