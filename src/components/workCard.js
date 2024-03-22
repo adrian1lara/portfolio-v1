40b989f9d5fb220e032f4e2b6968e45f6a9ec0dd
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 09:57:27 -0500

    update flex to grid for handle the responsive works display

--- src/components/workCard.js
+++ src/components/workCard.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function WorkCard({data}) {
     return (
-        <div className="flex flex-wrap max-w-[800px] justify-center space-x-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             {data.map((work, index) => (
-                <div key={index} className="max-w-[300px] ">
-                <Image src={work.img} height={"auto"} width={"auto"} className=""/>
+                <div key={index} className="max-w-[300px] p-2 hover:border-2 hover:border-green-300 hover:border-opacity-50 rounded">
+                <Image src={work.img} height={"200"} width={"300"} className="rounded"/>
                 <div className="p-4 text-center">
                     <h3 className="text-xl font-light capitalize">{work.title}</h3>
                     <p>{work.description}</p>
