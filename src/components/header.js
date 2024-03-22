6fbba5bbe53b41b3dbc22f8b96ebe02b2b7a586e
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 00:18:51 -0500

    update hover border and add logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,25 @@
 import Link from "next/link";
+import Logo from "./logo";
 
 export default function Header() {
     return (
-        <header className="flex  w-full fixed max-w-[800px] justify-center p-3 border-2">
+        <header className="flex  w-full fixed max-w-[800px] justify-between  p-3 backdrop-blur-md">
+            <Logo />
             <nav>
-                <ul className="flex space-x-2">
+                <ul className="flex space-x-5">
                     <li>
-                        <Link href={"/"} className="hover:border-b-2 ease-in duration-200 border-[#183D3D]">
+                        <Link href={"/"} className="hover:border-b-2 ease-in duration-200 border-green-300">
                             Home
                         </Link>
                     </li>
                     <li>
-                        <Link href={"/work"} className="hover:border-b-2 ease-in duration-200 border-[#183D3D]" >
+                        <Link href={"/work"} className="hover:border-b-2 ease-in duration-200 border-green-300" >
                             Work
                         </Link>
                         
                     </li>
                     <li>
-                        <Link href={"/posts"} className="hover:border-b-2 ease-in duration-200 border-[#183D3D]">
+                        <Link href={"/posts"} className="hover:border-b-2 ease-in duration-200 border-green-300">
                             Posts
                         </Link>
                     </li>
