895fccc6666d9f2a63a1abfcb9e5abaad8c11608
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 00:18:28 -0500

    add year and update styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return(
-        <footer>
-            <p>© Adrian Lara. All Rights Reserved</p>
+        <footer className="mt-10 p-2">
+            <p className="text-gray-500 text-sm">© 2024 Adrian Lara. All Rights Reserved.</p>
         </footer>
     )
 }
