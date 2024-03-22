130c343daacc7258e72a482eb0bd278a8b7b9ceb
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 09:58:04 -0500

    add alt for img

--- src/data/work.js
+++ src/data/work.js
@@ -1,18 +1,31 @@
-import chatty from "../assets/chatty.png"
+import chatty from  "../assets/chatty.png"
+import bloggy from "../assets/yourblog.png"
+import memberOnly from  "../assets/membersonly.png"
+import shopMe from "../assets/shopme.png"
+
 export const work = [
     {
         title: 'chatty',
         img: chatty,
+        alt: "chatty web image",
         description: "Chat Web App in real time"
     },
     {
         title: 'Bloggy',
-        img: chatty,
+        img: bloggy,
+        alt: "bloggy web image",
         description: "Personal blog",
     },
     {
         title: 'MembersOnly',
-        img: chatty,
-        description: "Web to post content and auth as a member"
+        img: memberOnly,
+        alt: "membersonly web image",
+        description: "Web with a membership community"
+    },
+    {
+        title: 'ShopMe',
+        img: shopMe,
+        alt: "Shopme web image",
+        description: "This a frontend web design consuming Fake Shop API"
     }
 ]
