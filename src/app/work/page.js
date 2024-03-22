9c27f450be1e0020d463e6e8861e53fa8772a579
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-22 09:56:16 -0500

    update h1 work styles

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -6,10 +6,8 @@ export default function WorkPage() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24 max-w-8xl text-white">
-            <div>
-                <h1>Work</h1>
-            </div>
-            <div>
+            <h1 className="text-xl border-b-2 border-green-300 border-opacity-50">Work</h1>
+            <div className="mt-10">
                 <WorkCard data={work}/>
             </div>
         </main>
