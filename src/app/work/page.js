7e6371374b6be9961fbeb3f00ce98a8e28afcf5b
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-23 10:44:18 -0500

    remove unnecesary html elements and add text-left

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -5,11 +5,9 @@ export default function WorkPage() {
 
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 max-w-8xl text-white">
-            <h1 className="text-xl border-b-2 border-green-300 border-opacity-50">Work</h1>
-            <div className="mt-10">
-                <WorkCard data={work}/>
-            </div>
+        <main className="flex min-h-screen flex-col  justify-between p-24 max-w-8xl text-white">
+            <h1 className="text-xl text-left border-b-2 border-green-300 border-opacity-50 w-fit">Work</h1>
+            <WorkCard data={work}/>
         </main>
     )
 }
