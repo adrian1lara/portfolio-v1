5004c700427352010f04db2b00c947f757574f1c
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-23 10:46:54 -0500

    add array of video elements

---
+++ src/data/videos.js
@@ -0,0 +1,31 @@
+import video1 from "@/assets/video1.png"
+import video2 from "@/assets/video2.png"
+import video3 from "@/assets/video3.png"
+import video4 from "@/assets/video4.png"
+
+export const videos = [
+    {
+        title: "Super chill coding vlog deploying my Frontend Chat-app with vercel",
+        img: video1,
+        alt: "miniature of youtube video",
+        url: "https://youtu.be/t_GegwLqE8c?si=Z8daflLXXUKNcil4"
+    },
+    {
+        title: "Chill Coding Vlog fixing one bug and deploy my API",
+        img: video2,
+        alt: "miniature of second youtube video",
+        url: "https://youtu.be/8gHDSn5kwn0?si=bPU53MA2zuI0Snjf"
+    },
+    {
+        title: "Chill Vlog coding CHATTY mi chat web app",
+        img: video3,
+        alt: "miniature of third youtube video",
+        url: "https://youtu.be/MLy9kMJfdh4?si=0W2RE0BvSA7rYEM2"
+    },
+    {
+        title: "Night Vlog Coding  + Valorant",
+        img: video4,
+        alt: "miniature of fourth youtube video",
+        url: "https://youtu.be/iV6b86a8ynw"
+    }
+]
