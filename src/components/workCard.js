178aa8f0045515b0b9852dc27733d559ad2e2609
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-23 10:46:28 -0500

    update div to a tag for link pages

--- src/components/workCard.js
+++ src/components/workCard.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function WorkCard({data}) {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="w-max mt-10 grid grid-cols-1  md:grid-cols-2 gap-5">
             {data.map((work, index) => (
-                <div key={index} className="max-w-[300px] p-2 hover:border-2 hover:border-green-300 hover:border-opacity-50 rounded">
-                <Image src={work.img} height={"200"} width={"300"} className="rounded"/>
+                <a key={index} className="max-w-[350px]  p-2 rounded ">
+                <Image src={work.img} width="auto" height="auto" className="rounded h-xl w-xl border-2 border-opacity-50 border-green-300" alt={work.alt}/>
                 <div className="p-4 text-center">
                     <h3 className="text-xl font-light capitalize">{work.title}</h3>
                     <p>{work.description}</p>
                 </div>
-            </div>
+            </a>
             ))} 
         </div>
     )
