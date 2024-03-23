c1b58296efa5954bfd99011d7eb16eef2de53359
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-23 10:51:26 -0500

    add url

--- src/data/work.js
+++ src/data/work.js
@@ -8,24 +8,28 @@ export const work = [
         title: 'chatty',
         img: chatty,
         alt: "chatty web image",
-        description: "Chat Web App in real time"
+        description: "Chat Web App in real time",
+        url: "https://chatty-branch.vercel.app/"
     },
     {
         title: 'Bloggy',
         img: bloggy,
         alt: "bloggy web image",
         description: "Personal blog",
+        url: "https://your-blog-three.vercel.app/"
     },
     {
         title: 'MembersOnly',
         img: memberOnly,
         alt: "membersonly web image",
-        description: "Web with a membership community"
+        description: "Web with a membership community",
+        url: "https://your-members-only.fly.dev/"
     },
     {
         title: 'ShopMe',
         img: shopMe,
         alt: "Shopme web image",
-        description: "frontend Web design consuming Fake Shop API"
+        description: "frontend Web design consuming Fake Shop API",
+        url: "https://1shopme.netlify.app/"
     }
 ]
