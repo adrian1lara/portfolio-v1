1f728ccd2fb9f0aac71acc0ea1be0bbadfc00232
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-23 10:50:54 -0500

    add target blank

--- src/components/videoCard.js
+++ src/components/videoCard.js
@@ -5,7 +5,7 @@ export default function VideoCard({data}) {
     return (
         <div className="w-max mt-10 grid grid-cols-1 md:grid-cols-2 gap-5">
             {data.map((video, index) => (
-                <a key={index} href={video.url}
+                <a key={index} href={video.url} target="_blank"
                  className="max-w-[350px] p-2 rounded">
                 <Image src={video.img} height={"auto"} width={"auto"} className="rounded h-xl w-xl" alt={video.alt}/>
                 <div className="p-4 text-center">
