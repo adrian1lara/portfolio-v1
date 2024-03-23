023b6022cea4c524b66db127a85210aa385329c3
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-23 10:45:22 -0500

    update margin top and padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return(
-        <footer className="mt-10 p-2">
+        <footer className="mt-5 p-3 ">
             <p className="text-gray-500 text-sm">© 2024 Adrian Lara. All Rights Reserved.</p>
         </footer>
     )
